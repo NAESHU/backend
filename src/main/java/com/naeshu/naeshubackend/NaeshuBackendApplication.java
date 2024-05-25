225e468ccbcb6b633fd26978d125b2d02159e3d6
hsjni0110 <hansukjinid@gmail.com>
2024-05-25 21:39:04 +0900

    init: 초기 프로젝트 구축

---
+++ src/main/java/com/naeshu/naeshubackend/NaeshuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.naeshu.naeshubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaeshuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaeshuBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/naeshu/naeshubackend/NaeshuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naeshu.naeshubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaeshuBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
